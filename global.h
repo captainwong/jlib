f5721d41e26ac0be861f6b0e9728c143e0a16367
win7_64_en <1281261856@qq.com>
2016-05-08 22:36:26 +0800

    added class auto_timer

--- global.h
+++ global.h
@@ -138,4 +138,25 @@ inline std::wstring get_exe_path()
 	return std::wstring(path).substr(0, pos);
 }
 
+
+class auto_timer : public boost::noncopyable
+{
+private:
+	int m_timer_id;
+	DWORD m_time_out;
+	HWND m_hWnd;
+
+public:
+
+	auto_timer(HWND hWnd, int timerId, DWORD timeout) : m_hWnd(hWnd), m_timer_id(timerId), m_time_out(timeout)
+	{
+		KillTimer(hWnd, m_timer_id);
+	}
+
+	~auto_timer()
+	{
+		SetTimer(m_hWnd, m_timer_id, m_time_out, nullptr);
+	}
+};
+
 };
