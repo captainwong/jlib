7d54be16c465ff9cd9d1391284a6cefb5a22cdf3
win7_64_en <1281261856@qq.com>
2016-07-03 14:15:46 +0800

    add url_codec.h

---
+++ url_codec.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include <cctype>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+inline std::string url_encode(const std::string& value) {
+    std::ostringstream escaped;
+    escaped.fill('0');
+    escaped << std::hex;
+
+    for (auto c : value) {
+        // Keep alphanumeric and other accepted characters intact
+        if (std::isalnum(c) || c == '-' || c == '_' || c == '.' || c == '~') {
+            escaped << c;
+            continue;
+        }
+
+        // Any other characters are percent-encoded
+        escaped << std::uppercase;
+        escaped << '%' << std::setw(2) << int((unsigned char) c);
+        escaped << std::nouppercase;
+    }
+
+    return escaped.str();
+}
