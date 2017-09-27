3380dbb44b2907495978fb54f57a7217eb136f79
win10_i5-4590 <1281261856@qq.com>
2017-09-27 08:28:42 +0800

    update

---
+++ mem_tool.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <memory>
+#include <functional>
+
+namespace jlib {
+
+//template <typename T>
+//struct deleter : public std::default_delete<T>
+//{};
+
+template <typename T>
+class auto_free
+{
+public:
+    typedef std::function<void(T*)> deleter;
+
+	explicit auto_free(T* data, deleter dter = {})
+		: data_(data), deleter_(dter)
+	{}
+
+    ~auto_free() {
+        if (data_ && deleter_) {
+            deleter_(data_);
+        }
+    }
+
+protected:
+    T* data_ = nullptr;
+	deleter deleter_ = nullptr;
+};
+
+}
--- win32/clipboard.h
+++ win32/clipboard.h
@@ -4,27 +4,51 @@
 
 namespace jlib{
 
-inline bool toClipboard(HWND hwnd, const std::string &s) 
+inline bool to_clipboard(const std::wstring &s)
 {
-    if (!OpenClipboard(hwnd)) return false;
-    if (!EmptyClipboard()) return false;
-    HGLOBAL hg = GlobalAlloc(GMEM_DDESHARE, s.size() + 1);
-    if (!hg) {
-        CloseClipboard();
-        return false;
-    }
-    LPVOID hMem = GlobalLock(hg);
-    if (!hMem) {
-        CloseClipboard();
-        return false;
-    }
-    memcpy(hMem, s.c_str(), s.size() + 1);
-    GlobalUnlock(hg);
-    SetClipboardData(CF_TEXT, hg);
-    CloseClipboard();
-    GlobalFree(hg);
-    return true;
+	if (!OpenClipboard(nullptr)) return false;
+	if (!EmptyClipboard()) return false;
+	size_t sz = (s.length() + 1) * 2;
+	HGLOBAL hg = GlobalAlloc(GMEM_MOVEABLE, sz);
+	if (!hg) {
+		CloseClipboard();
+		return false;
+	}
+	LPVOID hMem = GlobalLock(hg);
+	if (!hMem) {
+		CloseClipboard();
+		return false;
+	}
+	memset(hMem, 0, sz);
+	memcpy(hMem, s.data(), s.size() * 2);
+	GlobalUnlock(hg);
+	SetClipboardData(CF_UNICODETEXT, hg);
+	CloseClipboard();
+	GlobalFree(hg);
+	return true;
 }
-    
+
+inline std::wstring from_clipboard()
+{
+	std::wstring text;
+	do {
+		if (!OpenClipboard(nullptr))
+			break;
+
+		HANDLE hData = GetClipboardData(CF_UNICODETEXT);
+		if (hData == nullptr)
+			break;
+
+		auto pszText = static_cast<const wchar_t*>(GlobalLock(hData));
+		if (pszText == nullptr)
+			break;
+
+		text = pszText;
+		GlobalUnlock(hData);
+		CloseClipboard();
+	} while (false);
+	return text;
+}
+
     
 }
