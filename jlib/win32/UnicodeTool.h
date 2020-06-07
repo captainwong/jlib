33800c4cb1add580411acc61de7a941ded2b9ec0
i7-8700 <1281261856@qq.com>
2020-06-07 17:04:26 +0800

    test unicode tool

--- jlib/win32/UnicodeTool.h
+++ jlib/win32/UnicodeTool.h
@@ -9,147 +9,164 @@ namespace win32
 {
 
 
-inline bool mbcs_to_u16(const char* mbcs, wchar_t* u16buffer, size_t u16size) {
-	size_t request_size = MultiByteToWideChar(CP_ACP, 0, mbcs, -1, NULL, 0);
-	if (0 < request_size && request_size < u16size) {
-		MultiByteToWideChar(CP_ACP, 0, mbcs, -1, u16buffer, request_size);
-		return true;
-	}
-	return false;
-};
+//inline bool mbcs_to_u16(const char* mbcs, wchar_t* u16buffer, size_t u16size) {
+//	size_t request_size = MultiByteToWideChar(CP_ACP, 0, mbcs, -1, NULL, 0);
+//	if (0 < request_size && request_size < u16size) {
+//		MultiByteToWideChar(CP_ACP, 0, mbcs, -1, u16buffer, request_size);
+//		return true;
+//	}
+//	return false;
+//};
 
-inline std::wstring mbcs_to_u16(const std::string& mbcs) {
+inline std::wstring mbcs_to_utf16(const std::string& mbcs) {
 	std::wstring res = L"";
 	size_t request_size = MultiByteToWideChar(CP_ACP, 0, mbcs.c_str(), -1, NULL, 0);
 	if (0 < request_size) {
-		auto u16buffer = new wchar_t[request_size];
-		MultiByteToWideChar(CP_ACP, 0, mbcs.c_str(), -1, u16buffer, request_size);
-		res = u16buffer;
-		delete[] u16buffer;
+		auto buff = new wchar_t[request_size];
+		MultiByteToWideChar(CP_ACP, 0, mbcs.c_str(), -1, buff, request_size);
+		res = buff;
+		delete[] buff;
 	}
 	return res;
 }
 
-inline std::string u16_to_mbcs(const std::wstring& u16) {
+inline std::string utf16_to_mbcs(const std::wstring& u16) {
 	std::string res = "";
 	size_t request_size = WideCharToMultiByte(CP_ACP, 0, u16.c_str(), -1, 0, 0, 0, 0);
 	if (0 < request_size) {
-		auto mbcs_buffer = new char[request_size];
-		WideCharToMultiByte(CP_ACP, 0, u16.c_str(), -1, mbcs_buffer, request_size, 0, 0);
-		res = mbcs_buffer;
-		delete[] mbcs_buffer;
+		auto buff = new char[request_size];
+		WideCharToMultiByte(CP_ACP, 0, u16.c_str(), -1, buff, request_size, 0, 0);
+		res = buff;
+		delete[] buff;
 	}
 	return res;
 }
 
-
-inline std::string mbcs_to_utf8(const std::wstring& mbcs) {
+inline std::string utf16_to_utf8(const std::wstring& u16) {
 	std::string res = "";
-	size_t request_size = WideCharToMultiByte(CP_UTF8, 0, mbcs.c_str(), -1, 0, 0, 0, 0);
+	size_t request_size = WideCharToMultiByte(CP_UTF8, 0, u16.c_str(), -1, 0, 0, 0, 0);
 	if (0 < request_size) {
-		auto mbcs_buffer = new char[request_size];
-		WideCharToMultiByte(CP_UTF8, 0, mbcs.c_str(), -1, mbcs_buffer, request_size, 0, 0);
-		res = mbcs_buffer;
-		delete[] mbcs_buffer;
+		auto buff = new char[request_size];
+		WideCharToMultiByte(CP_UTF8, 0, u16.c_str(), -1, buff, request_size, 0, 0);
+		res = buff;
+		delete[] buff;
 	}
 	return res;
 }
 
+inline std::string mbcs_to_utf8(const std::string& mbcs) {
+	return utf16_to_utf8(mbcs_to_utf16(mbcs));
+}
 
-
-
-
-
-inline bool Utf16ToUtf8(const wchar_t* utf16String, char* utf8Buffer, size_t szBuff)
-{
-	size_t request_size = WideCharToMultiByte(CP_UTF8, 0, utf16String, -1, NULL, 0, 0, 0);
-	if (1 < request_size && request_size < szBuff) {
-		WideCharToMultiByte(CP_UTF8, 0, utf16String, -1, utf8Buffer, request_size, 0, 0);
-		return true;
+inline std::wstring utf8_to_utf16(const std::string u8) {
+	std::wstring res = L"";
+	size_t request_size = MultiByteToWideChar(CP_UTF8, 0, u8.c_str(), -1, NULL, 0);
+	if (0 < request_size) {
+		auto buff = new wchar_t[request_size];
+		MultiByteToWideChar(CP_UTF8, 0, u8.c_str(), -1, buff, request_size);
+		res = buff;
+		delete[] buff;
 	}
-	return false;
+	return res;
+}
+
+inline std::string utf8_to_mbcs(const std::string& u8) {
+	return utf16_to_mbcs(utf8_to_utf16(u8));
 }
 
 
-inline bool Utf8ToUtf16(const char* utf8String, wchar_t* utf16Buffer, size_t szBuff)
-{
-	size_t request_size = MultiByteToWideChar(CP_UTF8, 0, utf8String, -1, NULL, 0);
-	if (1 < request_size && request_size < szBuff) {
-		MultiByteToWideChar(CP_UTF8, 0, utf8String, -1, utf16Buffer, request_size);
-		return true;
-	}
-	return false;
-}
-
-/**************UNICODE-ANSI mutually transform**************************/
-// need be manuly delete
-__forceinline LPSTR Utf16ToAnsi(const wchar_t* const wSrc)
-{
-	char* pElementText;
-	int iTextLen;
-	iTextLen = WideCharToMultiByte(CP_ACP, 0, wSrc, -1, NULL, 0, NULL, NULL);
-	pElementText = new char[iTextLen + 1];
-	memset((void*)pElementText, 0, sizeof(char) * (iTextLen + 1));
-	::WideCharToMultiByte(CP_ACP, 0, wSrc, -1, pElementText, iTextLen, NULL, NULL);
-
-	return pElementText;
-}
-
-__forceinline BOOL Utf16ToAnsiUseCharArray(const wchar_t* const wSrc,
-										   char* ansiArr, DWORD ansiArrLen)
-{
-	int iTextLen = WideCharToMultiByte(CP_ACP, 0, wSrc, -1, NULL, 0, NULL, NULL);
-	if (static_cast<DWORD>(iTextLen) < ansiArrLen) {
-		memset((void*)ansiArr, 0, sizeof(char) * (iTextLen + 1));
-		::WideCharToMultiByte(CP_ACP, 0, wSrc, -1, ansiArr, iTextLen, NULL, NULL);
-		return TRUE;
-	}
-	return FALSE;
-}
-
-__forceinline wchar_t* AnsiToUtf16(PCSTR ansiSrc)
-{
-	wchar_t* pWide;
-	int iUnicodeLen = ::MultiByteToWideChar(CP_ACP,
-											0, ansiSrc, -1, NULL, 0);
-	pWide = new wchar_t[iUnicodeLen + 1];
-	memset(pWide, 0, (iUnicodeLen + 1) * sizeof(wchar_t));
-	::MultiByteToWideChar(CP_ACP, 0, ansiSrc, -1, (LPWSTR)pWide, iUnicodeLen);
-	return pWide;
-}
-
-__forceinline BOOL AnsiToUtf16Array(PCSTR ansiSrc, wchar_t* warr, int warr_len)
-{
-	int iUnicodeLen = ::MultiByteToWideChar(CP_ACP,
-											0, ansiSrc, -1, NULL, 0);
-	if (warr_len >= iUnicodeLen) {
-		memset(warr, 0, (iUnicodeLen + 1) * sizeof(wchar_t));
-		::MultiByteToWideChar(CP_ACP, 0, ansiSrc, -1, (LPWSTR)warr, iUnicodeLen);
-		return TRUE;
-	}
-	return FALSE;
-}
-
-__forceinline wchar_t* Utf8ToUtf16(PCSTR ansiSrc)
-{
-	wchar_t* pWide;
-	int  iUnicodeLen = ::MultiByteToWideChar(CP_UTF8,
-											 0, ansiSrc, -1, NULL, 0);
-	pWide = new  wchar_t[iUnicodeLen + 1];
-	memset(pWide, 0, (iUnicodeLen + 1) * sizeof(wchar_t));
-	::MultiByteToWideChar(CP_UTF8, 0, ansiSrc, -1, (LPWSTR)pWide, iUnicodeLen);
-	return pWide;
-}
-
-
-__inline const char* Utf16ToUtf8(const wchar_t* utf16, int& out_len)
-{
-	out_len = ::WideCharToMultiByte(CP_UTF8, 0, utf16, -1, NULL, 0, 0, 0);
-	char* p8 = new  char[out_len + 1];
-	memset(p8, 0, (out_len + 1) * sizeof(char));
-	::WideCharToMultiByte(CP_UTF8, 0, utf16, -1, p8, out_len, 0, 0);
-	return p8;
-}
+//
+//
+//inline bool Utf16ToUtf8(const wchar_t* utf16String, char* utf8Buffer, size_t szBuff)
+//{
+//	size_t request_size = WideCharToMultiByte(CP_UTF8, 0, utf16String, -1, NULL, 0, 0, 0);
+//	if (1 < request_size && request_size < szBuff) {
+//		WideCharToMultiByte(CP_UTF8, 0, utf16String, -1, utf8Buffer, request_size, 0, 0);
+//		return true;
+//	}
+//	return false;
+//}
+//
+//
+//inline bool Utf8ToUtf16(const char* utf8String, wchar_t* utf16Buffer, size_t szBuff)
+//{
+//	size_t request_size = MultiByteToWideChar(CP_UTF8, 0, utf8String, -1, NULL, 0);
+//	if (1 < request_size && request_size < szBuff) {
+//		MultiByteToWideChar(CP_UTF8, 0, utf8String, -1, utf16Buffer, request_size);
+//		return true;
+//	}
+//	return false;
+//}
+//
+///**************UNICODE-ANSI mutually transform**************************/
+//// need be manuly delete
+//__forceinline LPSTR Utf16ToAnsi(const wchar_t* const wSrc)
+//{
+//	char* pElementText;
+//	int iTextLen;
+//	iTextLen = WideCharToMultiByte(CP_ACP, 0, wSrc, -1, NULL, 0, NULL, NULL);
+//	pElementText = new char[iTextLen + 1];
+//	memset((void*)pElementText, 0, sizeof(char) * (iTextLen + 1));
+//	::WideCharToMultiByte(CP_ACP, 0, wSrc, -1, pElementText, iTextLen, NULL, NULL);
+//
+//	return pElementText;
+//}
+//
+//__forceinline BOOL Utf16ToAnsiUseCharArray(const wchar_t* const wSrc,
+//										   char* ansiArr, DWORD ansiArrLen)
+//{
+//	int iTextLen = WideCharToMultiByte(CP_ACP, 0, wSrc, -1, NULL, 0, NULL, NULL);
+//	if (static_cast<DWORD>(iTextLen) < ansiArrLen) {
+//		memset((void*)ansiArr, 0, sizeof(char) * (iTextLen + 1));
+//		::WideCharToMultiByte(CP_ACP, 0, wSrc, -1, ansiArr, iTextLen, NULL, NULL);
+//		return TRUE;
+//	}
+//	return FALSE;
+//}
+//
+//__forceinline wchar_t* AnsiToUtf16(PCSTR ansiSrc)
+//{
+//	wchar_t* pWide;
+//	int iUnicodeLen = ::MultiByteToWideChar(CP_ACP,
+//											0, ansiSrc, -1, NULL, 0);
+//	pWide = new wchar_t[iUnicodeLen + 1];
+//	memset(pWide, 0, (iUnicodeLen + 1) * sizeof(wchar_t));
+//	::MultiByteToWideChar(CP_ACP, 0, ansiSrc, -1, (LPWSTR)pWide, iUnicodeLen);
+//	return pWide;
+//}
+//
+//__forceinline BOOL AnsiToUtf16Array(PCSTR ansiSrc, wchar_t* warr, int warr_len)
+//{
+//	int iUnicodeLen = ::MultiByteToWideChar(CP_ACP,
+//											0, ansiSrc, -1, NULL, 0);
+//	if (warr_len >= iUnicodeLen) {
+//		memset(warr, 0, (iUnicodeLen + 1) * sizeof(wchar_t));
+//		::MultiByteToWideChar(CP_ACP, 0, ansiSrc, -1, (LPWSTR)warr, iUnicodeLen);
+//		return TRUE;
+//	}
+//	return FALSE;
+//}
+//
+//__forceinline wchar_t* Utf8ToUtf16(PCSTR ansiSrc)
+//{
+//	wchar_t* pWide;
+//	int  iUnicodeLen = ::MultiByteToWideChar(CP_UTF8,
+//											 0, ansiSrc, -1, NULL, 0);
+//	pWide = new  wchar_t[iUnicodeLen + 1];
+//	memset(pWide, 0, (iUnicodeLen + 1) * sizeof(wchar_t));
+//	::MultiByteToWideChar(CP_UTF8, 0, ansiSrc, -1, (LPWSTR)pWide, iUnicodeLen);
+//	return pWide;
+//}
+//
+//
+//__inline const char* Utf16ToUtf8(const wchar_t* utf16, int& out_len)
+//{
+//	out_len = ::WideCharToMultiByte(CP_UTF8, 0, utf16, -1, NULL, 0, 0, 0);
+//	char* p8 = new  char[out_len + 1];
+//	memset(p8, 0, (out_len + 1) * sizeof(char));
+//	::WideCharToMultiByte(CP_UTF8, 0, utf16, -1, p8, out_len, 0, 0);
+//	return p8;
+//}
 
 }
 }
---
+++ test/test_unicode/test_unicode.cpp
@@ -0,0 +1,74 @@
+﻿#include "../../jlib/win32/UnicodeTool.h"
+#include <stdio.h>
+#include <assert.h>
+
+using namespace jlib::win32;
+
+int main()
+{
+	// gb2312 ansi
+	// 不管cpp文件是什么编码，s1都是 CE D2 CA C7 D6 D0 B9 FA C8 CB
+	char s1[] = "我是中国人"; 
+	printf("s1 char:\n");
+	printf("%s\n", s1); // 可以打印
+	printf("raw %dbytes ", sizeof(s1) - 1);
+	for (int i = 0; i < sizeof(s1) - 1; i++) {
+		printf("%02X ", (unsigned char)s1[i]);
+	}
+	printf("\n\n");
+
+	// 6211 662F 4E2D 56FD 4EBA
+	wchar_t s2[] = L"我是中国人"; 
+	printf("s2 wchar_t:\n");
+	printf("%ls\n", s2); // 不可以打印
+	printf("raw %dbytes ", sizeof(s1) - 1);
+	for (int i = 0; i < sizeof(s2) / sizeof(wchar_t) - 1; i++) {
+		printf("%04X ", (unsigned short)s2[i]);
+	}
+	printf("\n\n");
+
+	// 6211 662F 4E2D 56FD 4EBA
+	auto s1u16 = mbcs_to_utf16(s1);
+	printf("s1u16 from mbcs_to_utf16 on s1:\n");
+	printf("%ls\n", s1u16.data()); // 不可以打印
+	printf("raw %zubytes ", s1u16.size() * sizeof(wchar_t));
+	for (size_t i = 0; i < s1u16.size(); i++) {
+		printf("%04X ", (unsigned short)s1u16[i]);
+	}
+	printf("\n\n");
+
+	// E6 88 91 E6 98 AF E4 B8 AD E5 9B BD E4 BA BA
+	auto s1u8 = mbcs_to_utf8(s1);
+	printf("s1u8 from mbcs_to_utf8 on s1:\n");
+	printf("%s\n", s1u8.data()); // 不可以打印
+	printf("raw %zubytes ", s1u8.size() * sizeof(char));
+	for (size_t i = 0; i < s1u8.size(); i++) {
+		printf("%02X ", (unsigned char)s1u8[i]);
+	}
+	printf("\n\n");
+
+	// CE D2 CA C7 D6 D0 B9 FA C8 CB
+	auto s2mbcs = utf16_to_mbcs(s2);
+	printf("s2mbcs from utf16_to_mbcs on s2:\n");
+	printf("%s\n", s2mbcs.data()); // 可以打印
+	printf("raw %zubytes ", s2mbcs.size() * sizeof(char));
+	for (size_t i = 0; i < s2mbcs.size(); i++) {
+		printf("%02X ", (unsigned char)s2mbcs[i]);
+	}
+	printf("\n\n");
+
+	// E6 88 91 E6 98 AF E4 B8 AD E5 9B BD E4 BA BA
+	auto s2u8 = utf16_to_utf8(s2);
+	printf("s2u8 from utf16_to_utf8 on s2:\n");
+	printf("%s\n", s2u8.data()); // 不可以打印
+	printf("raw %zubytes ", s2u8.size() * sizeof(char));
+	for (size_t i = 0; i < s2u8.size(); i++) {
+		printf("%02X ", (unsigned char)s2u8[i]);
+	}
+	printf("\n\n");
+
+	assert(s1u16 == s2);
+	assert(s1u8 == s2u8);
+	assert(s2mbcs == s1);
+
+}
