744f4c1c1f1f89eb5da5d691e2e9f6e4eec58848
i7-8700 <1281261856@qq.com>
2023-03-08 15:18:51 +0800

    fix logical drives

--- jlib/win32/info/logicaldrive.cpp
+++ jlib/win32/info/logicaldrive.cpp
@@ -36,8 +36,39 @@ std::vector<LogicalDrive> logical_drives()
 {
 	std::vector<LogicalDrive> drives;
 
-	wmi::Result result;
-	if (wmi::WmiBase::simpleSelect({ L"DeviceID", L"DriveType", L"FreeSpace", L"Size", L"VolumeName" }, L"Win32_LogicalDisk", L"where DriveType != 5", result)) {
+	wmi::Result devices;
+	if (wmi::WmiBase::simpleSelect({ L"DeviceID" }, L"Win32_LogicalDisk", L"where DriveType != 5", devices)) {
+		for (auto&& item : devices) {
+			std::wstring DeviceID = item[L"DeviceID"];
+			std::wstring Where = L"where DeviceID=\"" + DeviceID + L"\"";
+			LogicalDrive drive;
+			drive.device_id = utf16_to_mbcs(DeviceID);
+
+			wmi::Result DriveType, FreeSpace, Size, VolumeName;
+			if (!wmi::WmiBase::simpleSelect({ L"DriveType" }, L"Win32_LogicalDisk", Where, DriveType)) {
+				continue;
+			}
+			drive.drive_type = parseDriveType(DriveType[0][L"DriveType"]);
+
+			if (!wmi::WmiBase::simpleSelect({ L"FreeSpace" }, L"Win32_LogicalDisk", Where, FreeSpace)) {
+				continue;
+			}
+			drive.free_space = FreeSpace.empty() ? 0 : std::stoull(FreeSpace[0][L"FreeSpace"]);
+
+			if (!wmi::WmiBase::simpleSelect({ L"Size" }, L"Win32_LogicalDisk", Where, Size)) {
+				continue;
+			}
+			drive.size = Size.empty() ? 0 : std::stoull(Size[0][L"Size"]);
+
+			if (!wmi::WmiBase::simpleSelect({ L"VolumeName" }, L"Win32_LogicalDisk", Where, VolumeName)) {
+				continue;
+			}
+			drive.volume_name = VolumeName.empty() ? "" : utf16_to_mbcs(VolumeName[0][L"VolumeName"]);
+			drives.push_back(drive);
+		}
+	}
+
+	/*if (wmi::WmiBase::simpleSelect({ L"DeviceID", L"DriveType", L"FreeSpace", L"Size", L"VolumeName" }, L"Win32_LogicalDisk", L"where DriveType != 5", result)) {
 		for (auto&& item : result) {
 			LogicalDrive drive;
 			drive.device_id = utf16_to_mbcs(item[L"DeviceID"]);
@@ -51,7 +82,7 @@ std::vector<LogicalDrive> logical_drives()
 			drive.volume_name = utf16_to_mbcs(item[L"VolumeName"]);
 			drives.push_back(drive);
 		}
-	}
+	}*/
 
 	return drives;
 }
