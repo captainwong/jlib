efa5f3bc4a0f8594853e327f99182c0a98d0c427
r9-9950x <1281261856@qq.com>
2025-06-17 20:51:50 +0800

    upload

--- upload.sh
+++ upload.sh
@@ -3,17 +3,19 @@
 set -e
 
 if [ $# -ne 1 ]; then
-    DST_HOST='192.168.50.107'
+    DST_HOST='192.168.50.192'
 else
     DST_HOST=$1
 fi
 
-IGNORES="qt/Debug qt/Release"
+USER=jack
+IGNORES="qt/Debug/\* qt/Release/\* 3rdparty/\* obsolete/\* win32/\*"
 
 rm -f jlib.zip
 cd jlib
-zip -r ../jlib.zip * -x $IGNORES
+zip -r ../jlib.zip * -x qt/Debug/\* qt/Release/\* 3rdparty/\* obsolete/\* win32/\*
 cd ..
-scp jlib.zip root@$DST_HOST:/root/projects/jlib
+ssh ${USER}@$DST_HOST "rm -f /tmp/jlib.zip"
+scp jlib.zip ${USER}@$DST_HOST:/tmp
 rm -f jlib.zip
-ssh root@$DST_HOST "cd /root/projects/jlib && unzip -o jlib.zip -d jlib && rm -f jlib.zip"
+ssh ${USER}@$DST_HOST "cd /tmp && sudo unzip -o jlib.zip -d /usr/local/include/jlib && rm -f jlib.zip"
